6be12fb4617ca6acae3b4b98f6d2a943cd2d76ed
Francesco W <fwanderlingh@gmail.com>
2018-07-03 17:18:50 +0200

    added 3d 'VFType' option for virtual frame

--- include/ctrl_toolbox/VirtualFrame.h
+++ include/ctrl_toolbox/VirtualFrame.h
@@ -10,8 +10,7 @@
 
 #include <rml/RML.h>
 
-namespace ctb
-{
+namespace ctb {
 
 /**
  * @brief A simple virtual frame for smooth control
@@ -23,59 +22,61 @@ namespace ctb
  *
  * A mechanism is implemented to avoid that the virtual frame "runs away" too much from the tool frame.
  */
-class VirtualFrame
-{
+class VirtualFrame {
+
+  enum VFType { FullPose, Angular, Linear };
+
 private:
-
-	double sampleTime_;
-	double virtualFrameGain_;
+    double sampleTime_;
+    double virtualFrameGain_;
     double maximumAllowedDistance_;
-	Eigen::Vector6d toolToVirtualFrameError_;
-	Eigen::Vector6d virtualFrameToGoalError_;
-	Eigen::Vector6d normalizedVirtualFrameToGoalError_;
-	Eigen::Vector6d virtualFrameVelocity_;
-	Eigen::TransfMatrix wTv_;
+    Eigen::Vector6d toolToVirtualFrameError_;
+    Eigen::Vector6d virtualFrameToGoalError_;
+    Eigen::Vector6d normalizedVirtualFrameToGoalError_;
+    Eigen::Vector6d virtualFrameVelocity_;
+    Eigen::TransfMatrix wTv_;
+    VFType vftype_;
 
-	bool useErrorNorm;
+
+    bool useErrorNorm;
 
 public:
-
-	/**
+  /**
 	 * @brief Default constructor
 	 */
-	VirtualFrame();
+    VirtualFrame(VFType vft = FullPose);
 
-	/**
+  /**
 	 * @brief Set the integration sample time
 	 * Sets the integration sample time which is used by the Compute calls. This time should be the interval between two
 	 * consecutive calls to the Compute method, i.e. typically the sample time of the task itself, assuming the virtual frame
 	 * updated every run
 	 * @param[in] sampleTime integration sample time [s]
 	 */
-	void SetSampleTime(double sampleTime);
+    void SetSampleTime(double sampleTime);
 
-	/**
+    /**
 	 * @brief Set the virtual frame gain
 	 * The virtual frame will evolve from its initial position towards the goal frame
 	 * This gain sets the velocity of the convergence
 	 * @param[in] gain the gain value
 	 */
-	void SetGain(double gain);
+    void SetGain(double gain);
 
-	/**
+    /**
 	 * @brief Reset the virtual frame
 	 * Simply sets the virtual frame to the identity matrix and the error to zero
 	 */
-	void ResetState();
+    void ResetState();
 
-	/**
+    /**
 	 * @brief Reset the virtual frame to the given value
 	 * Sets the virtual frame to be the same as the given value
 	 * @param[in] wTv the transformation matrix of the virtual frame w.r.t. the world frame
 	 */
-	void ResetState(const Eigen::TransfMatrix& wTv);
+    void ResetState(const Eigen::TransfMatrix& wTv);
 
-	/**
+    /**
 	 * @brief Compute the new virtual frame position
 	 *
 	 * The method updates the position of the virtual frame \<v\> on the basis of the goal frame <g>
@@ -85,9 +86,9 @@ public:
 	 * @param[in] wTg the current goal frame position
 	 * @param[out] wTv the new virtual frame position
 	 */
-	void Compute(const Eigen::TransfMatrix& wTt, const Eigen::TransfMatrix& wTg, Eigen::TransfMatrix& wTv);
+    void Compute(const Eigen::TransfMatrix& wTt, const Eigen::TransfMatrix& wTg, Eigen::TransfMatrix& wTv);
 
-	/**
+    /**
 	 * @brief Compute the new virtual frame position
 	 * The method updates the position of the virtual frame <v> on the basis of the requested Cartesian velocity xdotbar
 	 * The current tool position <t> is used to prevent the virtual frame <v> from getting too far away from <t>
@@ -95,36 +96,33 @@ public:
 	 * @param[in] xdotbar the requested Cartesian velocity
 	 * @param[out] wTv the new virtual frame position
 	 */
-	void Compute(const Eigen::TransfMatrix& wTt, const Eigen::Vector6d& xdotbar, Eigen::TransfMatrix& wTv);
+    void Compute(const Eigen::TransfMatrix& wTt, const Eigen::Vector6d& xdotbar, Eigen::TransfMatrix& wTv);
 
-	void SetUseErrorNorm(bool useErrorNorm)
-	{
-		this->useErrorNorm = useErrorNorm;
-	}
+    void SetUseErrorNorm(bool useErrorNorm)
+    {
+        this->useErrorNorm = useErrorNorm;
+    }
 
     void SetMaximumAllowedDistance(double dist)
     {
         this->maximumAllowedDistance_ = dist;
     }
 
-	const Eigen::Vector6d& getVirtualFrameToGoalError() const
-	{
-		return virtualFrameToGoalError_;
-	}
+    const Eigen::Vector6d& getVirtualFrameToGoalError() const
+    {
+        return virtualFrameToGoalError_;
+    }
 
-	const Eigen::Vector6d& getToolToVirtualFrameError() const
-	{
-		return toolToVirtualFrameError_;
-	}
+    const Eigen::Vector6d& getToolToVirtualFrameError() const
+    {
+        return toolToVirtualFrameError_;
+    }
 
-	const Eigen::TransfMatrix& getwTvirt() const
-	{
-		return wTv_;
-	}
+    const Eigen::TransfMatrix& getwTvirt() const
+    {
+        return wTv_;
+    }
 };
-
 }
-
-
 
 #endif /* VIRTUAL_FRAME_H_ */
--- src/VirtualFrame.cc
+++ src/VirtualFrame.cc
@@ -7,81 +7,103 @@
 
 #include "VirtualFrame.h"
 
-namespace ctb
-{
+namespace ctb {
 
-VirtualFrame::VirtualFrame() :
-		useErrorNorm(false)
+VirtualFrame::VirtualFrame(VFType vft)
+    : vftype_(vft)
+    , useErrorNorm(false)
 {
-	virtualFrameGain_ = 0;
-	sampleTime_ = 0;
+    virtualFrameGain_ = 0;
+    sampleTime_ = 0;
     maximumAllowedDistance_ = 0.2;
 }
 
 void VirtualFrame::SetSampleTime(double sampleTime)
 {
-	sampleTime_ = sampleTime;
+    sampleTime_ = sampleTime;
 }
 
 void VirtualFrame::SetGain(double gain)
 {
-	virtualFrameGain_ = gain;
+    virtualFrameGain_ = gain;
 }
 
 void VirtualFrame::ResetState()
 {
-	toolToVirtualFrameError_.setZero();
+    toolToVirtualFrameError_.setZero();
 }
 
 void VirtualFrame::ResetState(const Eigen::TransfMatrix& wTv)
 {
-	wTv_ = wTv;
-	toolToVirtualFrameError_.setZero();
+    wTv_ = wTv;
+    toolToVirtualFrameError_.setZero();
 }
 
 void VirtualFrame::Compute(const Eigen::TransfMatrix& wTt, const Eigen::TransfMatrix& wTg, Eigen::TransfMatrix& wTv)
 {
     virtualFrameToGoalError_ = rml::CartesianError(wTv_, wTg);
 
+    if (useErrorNorm == true) {
+        normalizedVirtualFrameToGoalError_ = virtualFrameToGoalError_;
+        // std::cerr << "linErrNorm: " << virtualFrameToGoalError_.GetSecondVect3().norm() << std::endl;
+        double linErrNorm = virtualFrameToGoalError_.GetSecondVect3().norm();
+        if (linErrNorm != 0.0) {
+            Eigen::Vector3d linErrNormalized = virtualFrameToGoalError_.GetSecondVect3() / virtualFrameToGoalError_.GetSecondVect3().norm();
+            /// TODO: the linear error multiplier should be the linearErrorThreshold of the "CTRL::SingleArm" class
+            ///       multiplied by 2, to be formalized better
+            normalizedVirtualFrameToGoalError_.SetSecondVect3(linErrNormalized * 0.01);
+            virtualFrameVelocity_ = normalizedVirtualFrameToGoalError_ * virtualFrameGain_;
+        }
+    } else {
+        virtualFrameVelocity_ = virtualFrameToGoalError_ * virtualFrameGain_;
+    }
 
-	if (useErrorNorm == true) {
-		normalizedVirtualFrameToGoalError_ = virtualFrameToGoalError_;
-		//std::cerr << "linErrNorm: " << virtualFrameToGoalError_.GetSecondVect3().norm() << std::endl;
-		double linErrNorm = virtualFrameToGoalError_.GetSecondVect3().norm();
-		if (linErrNorm != 0.0) {
-			Eigen::Vector3d linErrNormalized = virtualFrameToGoalError_.GetSecondVect3() / virtualFrameToGoalError_.GetSecondVect3().norm();
-			/// TODO: the linear error multiplier should be the linearErrorThreshold of the "CTRL::SingleArm" class
-			///       multiplied by 2, to be formalized better
-			normalizedVirtualFrameToGoalError_.SetSecondVect3(linErrNormalized * 0.01);
-			virtualFrameVelocity_ = normalizedVirtualFrameToGoalError_ * virtualFrameGain_;
-		}
-	}else{
-		virtualFrameVelocity_ = virtualFrameToGoalError_ * virtualFrameGain_;
-	}
-
-	Compute(wTt, virtualFrameVelocity_, wTv);
+    Compute(wTt, virtualFrameVelocity_, wTv);
 }
 
 void VirtualFrame::Compute(const Eigen::TransfMatrix& wTt, const Eigen::Vector6d& xdotbar, Eigen::TransfMatrix& wTv)
 {
     toolToVirtualFrameError_ = rml::CartesianError(wTt, wTv);
 
-	//std::cout.precision(3);
-	//std::cout << "toolToVfError: " << toolToVirtualFrameError_.GetSecondVect3().norm() << "\t";
-	//std::cout << "VfToGoalError: " << virtualFrameToGoalError_.GetSecondVect3().norm() << std::endl;
+    //std::cout.precision(3);
+    //std::cout << "toolToVfError: " << toolToVirtualFrameError_.GetSecondVect3().norm() << "\t";
+    //std::cout << "VfToGoalError: " << virtualFrameToGoalError_.GetSecondVect3().norm() << std::endl;
 
-	/// virtual frame is getting away from tool frame
-	if ((toolToVirtualFrameError_ + xdotbar * sampleTime_).norm() > toolToVirtualFrameError_.norm()) {
-        virtualFrameVelocity_ = xdotbar * rml::DecreasingBellShapedFunction(0.00, maximumAllowedDistance_, 0, 1, toolToVirtualFrameError_.norm());
-	} else {
-		virtualFrameVelocity_ = xdotbar;
-	}
+    /// Virtual frame is getting away from tool frame
 
-	// move the virtual frame
-	wTv_ = wTv_.Integral(virtualFrameVelocity_, sampleTime_);
+    bool outOfReach = false;
+    Eigen::Vector3d error3d, xdotbar3d;
 
-	wTv = wTv_;
+    if (vftype_ == FullPose) {
+        if ((toolToVirtualFrameError_ + xdotbar * sampleTime_).norm() > toolToVirtualFrameError_.norm()) {
+            outOfReach = true;
+            virtualFrameVelocity_ = xdotbar * rml::DecreasingBellShapedFunction(0.00, maximumAllowedDistance_, 0, 1, toolToVirtualFrameError_.norm());
+        }
+    } else if (vftype_ == Angular) {
+        error3d = toolToVirtualFrameError_.GetFirstVect3();
+        xdotbar3d = xdotbar.GetFirstVect3();
+        if ((error3d + xdotbar3d * sampleTime_).norm() > error3d.norm()) {
+            outOfReach = true;
+            //virtualFrameVelocity_.SetSecondVect3(Eigen::Vector3d(0, 0, 0));
+            virtualFrameVelocity_.SetFirstVect3(xdotbar3d * rml::DecreasingBellShapedFunction(0.00, maximumAllowedDistance_, 0, 1, error3d.norm()));
+        }
+    } else if (vftype_ == Linear) {
+        error3d = toolToVirtualFrameError_.GetSecondVect3();
+        xdotbar3d = xdotbar.GetSecondVect3();
+        if ((error3d + xdotbar3d * sampleTime_).norm() > error3d.norm()) {
+            outOfReach = true;
+            //virtualFrameVelocity_.SetFirstVect3(Eigen::Vector3d(0, 0, 0));
+            virtualFrameVelocity_.SetSecondVect3(xdotbar3d * rml::DecreasingBellShapedFunction(0.00, maximumAllowedDistance_, 0, 1, error3d.norm()));
+        }
+    }
+
+    if (!outOfReach) {
+        virtualFrameVelocity_ = xdotbar;
+    }
+
+    // move the virtual frame
+    wTv_ = wTv_.Integral(virtualFrameVelocity_, sampleTime_);
+
+    wTv = wTv_;
 }
-
-
 }
