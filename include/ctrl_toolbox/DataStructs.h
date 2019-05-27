31970c29ff5b48b7ee517ff15eb4f2b0e45ba51f
carlottas <carlotta.sartore1@gmail.com>
2019-05-27 11:07:16 +0200

    daniele changes for adaptive control

--- include/ctrl_toolbox/DataStructs.h
+++ include/ctrl_toolbox/DataStructs.h
@@ -1,6 +1,8 @@
 #ifndef CTRL_TOOLBOX_DATASTRUCTS_H
 #define CTRL_TOOLBOX_DATASTRUCTS_H
 
+#include <rml/RML.h>
+
 namespace ctb {
 
 struct PIDGains {
@@ -21,6 +23,27 @@ struct LatLong {
         , longitude(0.0)
     {
     }
+};
+
+struct ADPGains {
+    Eigen::Vector6d Kl = {}; //!< Pose error Proportional gain
+    Eigen::Vector9d Kg = {}; //!< Dynamic parameters update law gain
+    Eigen::Vector6d Ks = {}; //!< Velocity error Proportional gain
+    Eigen::Vector9d gamma = {};
+    Eigen::Vector3d upGB_m = {};
+    Eigen::Vector3d lowGB_m = {};
+    Eigen::Vector3d up_f = {};
+    Eigen::Vector3d low_f = {};
+    Eigen::Vector3d up_m = {};
+    Eigen::Vector3d low_m = {};
+    std::vector<bool> en_integration = {};
+
+
+    //double Kff = { 0.0 }; //!< Feed-forward gain
+    //double N = { 0.0 }; //!< Maximum gain for derivative part
+    //double Tr = { 0.0 }; //!< Tracking time constant for anti-windup
+
+    ADPGains() = default;
 };
 
 }
