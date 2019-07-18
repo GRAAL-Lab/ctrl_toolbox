71ba4f56f15f83e3a8b7d4af64d0ae89842080ec
carlottas <carlotta.sartore1@gmail.com>
2019-07-18 17:28:56 +0200

    added saturation for line sight position

--- src/LineOfSight.cpp
+++ src/LineOfSight.cpp
@@ -11,24 +11,44 @@ void LineOfSight::Compute(const Eigen::TransfMatrix& wTt, const Eigen::TransfMat
 {
     // Computing trajectory and norm to the trajectory
     trajectory_ = wTg.GetTransl() - wTvInitial_.GetTransl();
+    std::cout << "wTg = " << wTg.GetTransl().transpose() << std::endl;
+    std::cout << "wTv_initial = " << wTvInitial_.GetTransl().transpose() << std::endl;
+    std::cout << "wTt_current = " << wTt.GetTransl().transpose() << std::endl;
+    Eigen::Vector3d wTt_traslation = wTt.GetTransl();
+    Eigen::Vector3d wTt_initial_traslation = wTvInitial_.GetTransl();
     if (computeTrajectoryProjector_ == OnPlane) {
         Eigen::Vector3d projectorVector_worldFrame = wRp_.col(2);
         Eigen::Matrix3d P_OnPlane
             = (Eigen::Matrix3d::Identity() - projectorVector_worldFrame * projectorVector_worldFrame.transpose());
         trajectory_ = P_OnPlane * trajectory_;
+        wTt_traslation = P_OnPlane * wTt_traslation;
+        wTt_initial_traslation = P_OnPlane * wTt_initial_traslation;
     }
     Eigen::Vector3d normTrajectory = trajectory_.normalized();
 
     // Computing closest point to trajectory
     Eigen::MatrixXd P = normTrajectory * normTrajectory.transpose();
-    Eigen::Vector3d closestPoint = P * wTt.GetTransl();
-    std::cout << "closestPoint = "<< closestPoint.transpose()<<std::endl;
+    Eigen::Vector3d closestPoint = P * wTt_traslation + wTt_initial_traslation;
+    std::cout << "closestPoint = " << closestPoint.transpose() << std::endl;
 
     // Incrementing closest point
     Eigen::Vector3d increment = normTrajectory * delta_;
     Eigen::Vector3d newPosition = closestPoint + increment;
-    std::cout <<"increment = "<<increment.transpose()<<std::endl;
-    std::cout << "newPosition = "<< newPosition.transpose()<<std::endl;
+    // problem if negative not working! TODO
+    for (int i = 0; i < 3; i++) {
+        if (wTg.GetTransl()(i) > 0.0) {
+            rml::SaturateScalar(wTg.GetTransl()(i), newPosition(i));
+        }
+        else {
+            if(newPosition(i)<0.0){
+                double newPositionAbsoluteValue = std::fabs(newPosition(i));
+                rml::SaturateScalar(std::fabs(wTg.GetTransl()(i)),newPositionAbsoluteValue);
+                newPosition(i) = - newPositionAbsoluteValue;
+            }
+        }
+    }
+    std::cout << "increment = " << increment.transpose() << std::endl;
+    std::cout << "newPosition = " << newPosition.transpose() << std::endl;
 
     // Definition of new goal
     wTv.SetTransl(newPosition);
