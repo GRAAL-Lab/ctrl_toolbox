3c33764bb5f99f288acc72a33e1f7e62b2e0f343
Francesco Wanderlingh <fwanderlingh@gmail.com>
2022-03-25 11:48:33 +0100

    Added a library include header

---
+++ include/ctrl_toolbox/ctrl_toolbox.hpp
@@ -0,0 +1,9 @@
+#ifndef CTRL_TOOLBOX_HPP
+#define CTRL_TOOLBOX_HPP
+
+#include "ctrl_toolbox/HelperFunctions.h"
+#include "ctrl_toolbox/virtual_frame/VirtualFrame.h"
+#include "ctrl_toolbox/pid/DigitalPID.h"
+#include "ctrl_toolbox/kalman_filter/ExtendedKalmanFilter.h"
+
+#endif // CTRL_TOOLBOX_HPP
